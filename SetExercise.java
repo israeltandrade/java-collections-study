f4f0e7e3d80cf4ed3b1c24ce2d482b113d71db5c
Israel Andrade DEV <israeltandrade@hotmail.com>
2024-08-04 09:37:17 -0300

    First set implementation exercise

---
+++ SetExercise.java
@@ -0,0 +1,79 @@
+package collections_exercises;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+
+public class SetExercise {
+
+	public static void main (String args[]) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Set<Integer> courseA = new HashSet<>();
+		Set<Integer> courseB = new HashSet<>();
+		Set<Integer> courseC = new HashSet<>();
+		
+		
+		System.out.print("How many students for course A? ");
+		int studentsNumber = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println();
+		
+		for (int i = 0; i < studentsNumber; i++) {
+			System.out.print("Provide the ID (two digits) of the ");
+			System.out.print(i + 1);
+			System.out.print("st student: ");
+			courseA.add(sc.nextInt());
+			sc.nextLine();
+		}
+		
+		System.out.println();
+		
+		System.out.print("How many students for course B? ");
+		studentsNumber = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println();
+		
+		for (int i = 0; i < studentsNumber; i++) {
+			System.out.print("Provide the ID (two digits) of the ");
+			System.out.print(i + 1);
+			System.out.print("st student: ");
+			courseB.add(sc.nextInt());
+			sc.nextLine();
+		}
+		
+		System.out.println();
+		
+		System.out.print("How many students for course C? ");
+		studentsNumber = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println();
+		
+		for (int i = 0; i < studentsNumber; i++) {
+			System.out.print("Provide the ID (two digits) of the ");
+			System.out.print(i + 1);
+			System.out.print("st student: ");
+			courseC.add(sc.nextInt());
+			sc.nextLine();
+		}
+		
+		System.out.println();
+
+		courseA.addAll(courseB);
+		courseA.addAll(courseC);
+		
+		System.out.println(courseA.toString());
+		
+		System.out.println("Total students: " + courseA.size());
+		
+		sc.close();
+				
+	}
+	
+}
